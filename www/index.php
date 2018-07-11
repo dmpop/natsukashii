c45f1230bd256be5c01e3846fd991a49996aae1a
Dmitri Popov <dmpop@linux.com>
2018-07-11 16:22:57 +0200

    Improved styling and index.php

--- www/index.php
+++ www/index.php
@@ -12,19 +12,26 @@
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 	<meta name="viewport" content="width=device-width">
 	<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/css?family=Markazi+Text" rel="stylesheet" type='text/css'>
 	<link rel="shortcut icon" href="favicon.ico" />
-	<link rel="stylesheet" href="https://unpkg.com/sakura.css/css/sakura-earthly.css" type="text/css">
+	<link rel="stylesheet" href="sakura-dark.css" type="text/css">
+    <title>Natsukashii</title>
     </head>
     <body>
 	<h1>Stroll down Memory Lane</h1>
+    <hr>
 	<p>You took these photos exactly one year ago.</p>
-	<?php
-	$files = glob("photos/*.*");
-	for ($i = 0; $i < count($files); $i++) {
-            $photo = $files[$i];
-            echo '<img style="border-radius:9px;" src="' . $photo . '" alt="" width="800"/>' . "<br /><br />";
-	}
-	?>
-	<p>Made with <a href="https://gitlab.com/dmpop/natsukashii">Natsukashii</a>.
+    <?php
+     $files = glob("photos/*.*");
+     for ($i = 0; $i < count($files); $i++) {
+         $photo = $files[$i];
+         $exif = exif_read_data($photo, 0, true);
+         echo '<img src="' . $photo . '" alt="" width="800"/>' . "<br /><br />";
+         echo '<small>'.$exif['COMMENT']['0'].'</small>';
+         echo '<p></p>';
+     }
+    ?>
+    <hr>
+	<p>Made with <a href="https://gitlab.com/dmpop/natsukashii">Natsukashii</a>
     </body>
 </html>
